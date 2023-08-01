a68ffe44732bf16338c960073542978dc6b6e286
mour77 <mour77@users.noreply.github.com>
2023-08-01 14:05:51 +0300

    διαφορες προσθηκες

---
+++ app/src/main/java/com/example/myvehicleinfojava/classes/Notification.java
@@ -0,0 +1,40 @@
+package com.example.myvehicleinfojava.classes;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.ServerTimestamp;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Notification {
+
+    public String id;
+    public String description;
+    public String uid;
+    @ServerTimestamp
+    public Timestamp date;
+    public boolean isSent;
+
+
+
+
+    public String getDate() {
+        if (date == null)
+            return "";
+        Date d = (date.toDate());
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", new Locale("el", "GR") );
+        return dateFormat.format(d);
+
+    }
+
+
+    public static class colNames{
+        public static final String DESCRIPTION = "description";
+        public static final String IS_SENT = "isSent";
+        public static final String DATE = "date";
+        public static final String UID = "uid";
+
+    }
+}
---
+++ app/src/main/java/com/example/myvehicleinfojava/dialogs/AddNotificationDialog.java
@@ -0,0 +1,76 @@
+package com.example.myvehicleinfojava.dialogs;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.myvehicleinfojava.R;
+import com.example.myvehicleinfojava.Utils;
+import com.example.myvehicleinfojava.classes.Notification;
+import com.example.myvehicleinfojava.listeners.GeneralListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddNotificationDialog {
+
+    public static void show(Activity act , GeneralListener listener){
+        AlertDialog.Builder builder = new AlertDialog.Builder(act);
+        builder.setTitle("Προσθήκη ειδοποίησης");
+
+        // set the custom layout
+        final View customLayout = act.getLayoutInflater().inflate(R.layout.add_notification_dialog, null);
+        builder.setView(customLayout);
+        EditText descriptionET = customLayout.findViewById(R.id.descriptionET);
+        TextView dateTV = customLayout.findViewById(R.id.dateTV);
+        dateTV.setText(Utils.getCurrentDate());
+        Utils.setDatePicker(act,dateTV);
+
+        // add a button
+        builder.setPositiveButton("OK", (dialog, which) -> {
+
+            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            FirebaseUser user = mAuth.getCurrentUser();
+
+            Map<String, Object> notificationMap = new HashMap<>();
+            notificationMap.put(Notification.colNames.UID, user.getUid());
+            notificationMap.put(Notification.colNames.DESCRIPTION, descriptionET.getText().toString());
+            notificationMap.put(Notification.colNames.DATE, Utils.convertToTimestamp(dateTV.getText().toString()));
+            notificationMap.put(Notification.colNames.IS_SENT, false);
+
+
+
+            db.collection("Notifications").document()
+                    .set(notificationMap)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(act, R.string.successful_save, Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(act, R.string.error_save, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        });
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
